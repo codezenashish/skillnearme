45cffea0379adbeaca1df57601935b098d5d7fca
codezenashish <ixtashish@gmail.com>
2026-06-21 23:44:15 +0530

    feat: setup project structure with Tailwind, layouts, and initial landing page sections

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import HeroSection from "@/src/features/landing/hero/components/HeroSection";
+
+const page = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+};
+export default page;
---
+++ src/components/MapBackground.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function MapBackground() {
+  return (
+    <div className="absolute inset-0 opacity-55 pointer-events-none z-0 select-none">
+      <Image
+        src="/Frame.svg"
+        alt="Local Map Background"
+        fill
+        className="object-cover"
+        priority
+      />
+    </div>
+  );
+}
