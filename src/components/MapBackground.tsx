a93b42f0a786d9eb054553f38418df4b10d48242
codezenashish <ixtashish@gmail.com>
2026-07-06 23:53:25 +0530

    feat: update background image and implement scroll-aware responsive navigation bar styling

--- src/components/MapBackground.tsx
+++ src/components/MapBackground.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
+import bg from "@/src/assets/bg.jpg";
 
 export default function MapBackground() {
   return (
     <div className="absolute inset-0 opacity-55  pointer-events-none z-0 select-none">
       <Image
-        src="/Frame.svg"
+        src={bg}
         alt="Local Map Background"
         fill
-        className="object-cover "
-        priority
+        className="object-cover"
       />
     </div>
   );
